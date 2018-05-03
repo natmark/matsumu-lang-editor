d259f561a43e5c551d1bfe03159ba8d10e3a2945
natmark <natmark0918@gmail.com>
2018-05-03 13:48:17 +0900

    :sparkles: Add editor view

--- app.rb
+++ app.rb
@@ -7,6 +7,16 @@ require_relative 'models/init'
 
 class Server < Sinatra::Base
     get '/' do
+        redirect '/editor'
+    end
+
+    get '/editor' do
+        haml :index
+    end
+
+    post '/editor' do
+        @plain = params['plain']
+        @brain = Matsumura.new.generate(@plain)
         haml :index
     end
 end
